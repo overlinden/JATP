ab170102b2be4167f9bdbebc2d098f85475bdb83
Oliver Verlinden <oliver@wps-verlinden.de>
2015-05-19 08:47:55 +0200

    Added tests into repository

---
+++ JATP/src/test/java/de/wpsverlinden/jatp/JatpTest.java
@@ -0,0 +1,79 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package de.wpsverlinden.latencyprofiler;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author OVerlinden
+ */
+public class JatpTest {
+    
+    @Test(expected = RuntimeException.class)
+    public void endMeasureThrowsExceptionOnCapacityExceeded() {
+        Jatp profiler = new Jatp(1);
+        
+        profiler.startMeasure();
+        profiler.endMeasure();
+        
+        profiler.startMeasure();
+        profiler.endMeasure();
+    }
+    
+    @Test(expected = RuntimeException.class)
+    public void endNotRunningMeasurementCausesException() {
+        Jatp profiler = new Jatp(0);
+        profiler.endMeasure();
+    }
+    
+    @Test(expected = RuntimeException.class)
+    public void startRunningMeasurementCausesException() {
+        Jatp profiler = new Jatp(2);
+        profiler.startMeasure();
+        profiler.startMeasure();
+    }
+    
+    @Test
+    public void numberOfProbesIsCorrect() {
+        Jatp profiler = new Jatp(10);
+        
+        profiler.startMeasure();
+        profiler.endMeasure();
+        
+        profiler.startMeasure();
+        profiler.endMeasure();
+        
+        profiler.startMeasure();
+        profiler.endMeasure();
+        
+        Stats stats = profiler.getStats();
+        assertEquals(3, stats.getNumOfProbes());
+    }
+    
+    @Test
+    public void maxGreaterAvgGreaterMin() throws InterruptedException {
+        Jatp profiler = new Jatp(10);
+        
+        profiler.startMeasure();
+        Thread.sleep(100);
+        profiler.endMeasure();
+        
+        profiler.startMeasure();
+        Thread.sleep(200);
+        profiler.endMeasure();
+        
+        profiler.startMeasure();
+        Thread.sleep(300);
+        profiler.endMeasure();
+        
+        Stats stats = profiler.getStats();
+        assertTrue(stats.getUsForBestRun() > 90000 && stats.getUsForBestRun() < 110000);
+        assertTrue(stats.getUsForAvgRun() > 190000 && stats.getUsForAvgRun() < 210000);
+        assertTrue(stats.getUsForWorstRun() > 290000 && stats.getUsForBestRun() < 310000);
+    }
+}
---
+++ JATP/src/test/java/de/wpsverlinden/jatp/StatsTest.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package de.wpsverlinden.latencyprofiler;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author OVerlinden
+ */
+public class StatsTest {
+    
+    @Test
+    public void contructorAndGetterWorkingProperly() {
+        Stats s = new Stats(1, 2, 3, 4, 5, 6, 7, 8, 9);
+        assertEquals(1, s.getNumOfProbes());
+        assertEquals(2, s.getUsFor50Percentile(), 0);
+        assertEquals(3, s.getUsFor90Percentile(), 0);
+        assertEquals(4, s.getUsFor99Percentile(), 0);
+        assertEquals(5, s.getUsFor999Percentile(), 0);
+        assertEquals(6, s.getUsFor9999Percentile(), 0);
+        assertEquals(7, s.getUsForBestRun(), 0);
+        assertEquals(8, s.getUsForAvgRun(), 0);
+        assertEquals(9, s.getUsForWorstRun(), 0);
+    }
+    
+    @Test
+    public void printsRelevantInformation() {
+        ByteArrayOutputStream output = new ByteArrayOutputStream();
+        Stats s = new Stats(1, 2, 3, 4, 5, 6, 7, 8, 9);
+        s.print(new PrintStream(output));
+        assertEquals("Latency measured with 1 probe(s): \n" +
+                    " 2,00 us for 50 percentile\n" +
+                    " 3,00 us for 90 percentile\n" +
+                    " 4,00 us for 99 percentile\n" +
+                    " 5,00 us for 99.9 percentile\n" +
+                    " 6,00 us for 99.99 percentile\n" +
+                    " 7,00 us best run\n" +
+                    " 8,00 us avg run\n" +
+                    " 9,00 us worst run\n", 
+                output.toString());
+    }
+}
