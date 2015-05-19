18b54dbf603cb6281ae2de214e6822b114bf0b9c
Oliver Verlinden <oliver@wps-verlinden.de>
2015-05-19 08:48:59 +0200

    Fixed package declarations

--- JATP/src/main/java/de/wpsverlinden/jatp/Jatp.java
+++ JATP/src/main/java/de/wpsverlinden/jatp/Jatp.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package de.wpsverlinden.latencyprofiler;
+package de.wpsverlinden.jatp;
 
 import java.util.Arrays;
 
--- JATP/src/test/java/de/wpsverlinden/jatp/StatsTest.java
+++ JATP/src/test/java/de/wpsverlinden/jatp/StatsTest.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package de.wpsverlinden.latencyprofiler;
+package de.wpsverlinden.jatp;
 
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
