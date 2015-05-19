7c5d702c791997e290944d3b95efd624fcb54a55
Oliver Verlinden <oliver@wps-verlinden.de>
2015-05-19 08:51:13 +0200

    Updated license information

--- JATP/src/test/java/de/wpsverlinden/jatp/StatsTest.java
+++ JATP/src/test/java/de/wpsverlinden/jatp/StatsTest.java
@@ -1,7 +1,19 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * JATP - A simple Java Application Timing Profiler
+ * Copyright (C) 2015 Oliver Verlinden (http://wps-verlinden.de)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 package de.wpsverlinden.jatp;
 
@@ -10,10 +22,6 @@ import java.io.PrintStream;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
-/**
- *
- * @author OVerlinden
- */
 public class StatsTest {
     
     @Test
